f20d5bf9eca6d80840c3b1c582fc046ebd386b5b
kenann <kenann.pang@foodpanda.com>
2023-03-16 11:01:05 +0800

    setup: adding colours to standardise palette towards foodpanda app

---
+++ learning-swft/Extensions.swift
@@ -0,0 +1,11 @@
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let vistawhite = Color("vistawhite")
+    static let cerisered = Color("cerisered")
+    static let silver = Color("silver")
+    static let dustygray = Color("dustygray")
+    static let fantasy = Color("fantasy")
+//not sure what this is for    //static let systemBackground = Color(uiColor: .systemBackground)
+}
